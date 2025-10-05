664821677be3959c2cb53ed70e34af2d8f829630
Johanna Winter <johanna.winter@gmx.de>
2025-10-05 19:39:55 +0200

    update log type program

---
+++ js-conditions/js-conditions-and-booleans_log-type/js/index.js
@@ -0,0 +1,31 @@
+const data = -5;
+
+switch (typeof data) {
+  case "undefined":
+    console.log("undefined!");
+    break;
+  case "number":
+    if (isNaN(data)) {
+      console.log("not a number!");
+    } else {
+      console.log("number!");
+    }
+    break;
+  case "string":
+    console.log("string!");
+    break;
+  case "boolean":
+    console.log("boolean!");
+    break;
+  case "object":
+    if (data === null) {
+      console.log("null!");
+    } else if (Array.isArray(data)) {
+      console.log("array!");
+    } else {
+      console.log("object!");
+    }
+    break;
+  default:
+    console.log("I have no idea!");
+}
