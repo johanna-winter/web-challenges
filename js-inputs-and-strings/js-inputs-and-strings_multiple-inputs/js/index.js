b40a4c4cd589fbd0c551b7ec5491309305ddd4a0
Johanna Winter <johanna.winter@gmx.de>
2025-10-07 16:37:47 +0200

    solved all 3 parts

--- js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -4,7 +4,9 @@ const firstCopyInput = document.querySelector("[data-js=first-copy-input]");
 const secondCopyInput = document.querySelector("[data-js=second-copy-input]");
 const copyButton = document.querySelector("[data-js=copy-button]");
 
-// --v-- Write your code here --v--
+copyButton.addEventListener("click", () => {
+  secondCopyInput.value = firstCopyInput.value;
+});
 
 // --^-- End of Task 1 --^--
 
@@ -20,7 +22,10 @@ const copyInUppercaseButton = document.querySelector(
   "[data-js=copy-uppercase-button]"
 );
 
-// --v-- Write your code here --v--
+copyInUppercaseButton.addEventListener("click", () => {
+  secondCopyInUppercaseInput.value =
+    firstCopyInUppercaseInput.value.toUpperCase();
+});
 
 // --^-- End of Task 2 --^--
 
@@ -36,6 +41,10 @@ const switchValueButton = document.querySelector(
   "[data-js=switch-value-button]"
 );
 
-// --v-- Write your code here --v--
+switchValueButton.addEventListener("click", () => {
+  const temporaryValue = firstSwitchValueInput.value;
+  firstSwitchValueInput.value = secondSwitchValueInput.value;
+  secondSwitchValueInput.value = temporaryValue;
+});
 
 // --^-- End of Task 3 --^--
