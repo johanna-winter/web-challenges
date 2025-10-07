3a02a558f2e2ea9018a3be29f1d46cda6d53c5cc
Johanna Winter <johanna.winter@gmx.de>
2025-10-07 15:37:45 +0200

    solved uppercase task

--- js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,13 @@
 console.clear();
+
+// Assign elements to variables
+const firstInput = document.querySelector('[data-js="first-input"]');
+const buttonUppercase = document.querySelector('[data-js="button-uppercase"]');
+
+console.log(firstInput);
+console.log(buttonUppercase);
+
+// Add an event listener to button
+buttonUppercase.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toUpperCase();
+});
