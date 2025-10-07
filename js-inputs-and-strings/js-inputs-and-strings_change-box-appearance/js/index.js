8da6322362770c30a26d1ce9fdd26436c1e6d4aa
Johanna Winter <johanna.winter@gmx.de>
2025-10-07 22:46:45 +0200

    changed box appearance

--- js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,20 @@
 console.clear();
+
+const colorInput = document.querySelector('[data-js="input-color"]');
+const radiusInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+const box = document.querySelector('[data-js="box"]');
+
+colorInput.addEventListener("input", () => {
+  box.style.backgroundColor = `hsl(${colorInput.value} 100% 50%)`;
+});
+
+radiusInput.addEventListener("input", () => {
+  const radius = radiusInput.value;
+  box.style.borderRadius = `${radius}px`;
+});
+
+rotationInput.addEventListener("input", () => {
+  const deg = rotationInput.value;
+  box.style.transform = `rotate(${deg}deg)`;
+});
