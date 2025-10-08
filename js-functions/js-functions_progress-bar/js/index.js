80ea30ebe36816a2aa7e608d3d5772c2277aa880
Johanna Winter <johanna.winter@gmx.de>
2025-10-08 10:35:28 +0200

    update solution

--- js-functions/js-functions_progress-bar/js/index.js
+++ js-functions/js-functions_progress-bar/js/index.js
@@ -2,4 +2,14 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+  const scrollYPosition = window.scrollY;
+  const innerHeight = window.innerHeight;
+  const webpageHeight = document.body.clientHeight;
+  return (scrollYPosition / (webpageHeight - innerHeight)) * 100;
+}
+
+document.addEventListener("scroll", () => {
+  const scrollPosition = calculateScrollPercentage();
+  progressBar.style.width = `${scrollPosition}%`;
+});
