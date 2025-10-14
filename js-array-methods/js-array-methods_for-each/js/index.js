a11e660c80454fd531dabca1755ffc18ce355760
Johanna Winter <johanna.winter@gmx.de>
2025-10-14 15:44:21 +0200

    add color boxes

--- js-array-methods/js-array-methods_for-each/js/index.js
+++ js-array-methods/js-array-methods_for-each/js/index.js
@@ -19,4 +19,20 @@ const colors = [
   "#A5A5A5",
 ];
 
-colors.forEach();
+// colors.forEach((color) => {
+//   const div = document.createElement("div");
+//   div.classList.add("color-box");
+//   div.style.backgroundColor = color;
+//   document.body.append(div);
+// });
+
+// Task 2
+
+function renderColorBox(color) {
+  const div = document.createElement("div");
+  div.classList.add("color-box");
+  div.style.backgroundColor = color;
+  document.body.append(div);
+}
+
+colors.forEach(renderColorBox);
