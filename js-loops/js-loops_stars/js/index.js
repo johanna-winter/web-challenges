cb01d914e06ca13876ec01269c56442453a01b9c
Johanna Winter <johanna.winter@gmx.de>
2025-10-14 15:14:33 +0200

    solve part 1

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -5,7 +5,7 @@ const starContainer = document.querySelector('[data-js="star-container"]');
 //1. render a single star (create img and append)
 //2. use a for loop to do the same 5 times
 
-const singleStar
+const star = document.createElement("star");
 
 function renderStars() {
   // Reset the star container before re-rendering stars
