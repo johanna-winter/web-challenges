de9dc55178269289b21315689636bf6a20b1ee1c
Johanna Winter <johanna.winter@gmx.de>
2025-10-14 23:40:49 +0200

    update handle form challenge

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,8 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event.target);
+});
