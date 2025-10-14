a835feb388b0fdcd8b9c4dcea45186c494b5f76b
Johanna Winter <johanna.winter@gmx.de>
2025-10-14 12:51:47 +0200

    initial commit

---
+++ js-loops/js-loops_stars/js/index.js
@@ -0,0 +1,19 @@
+console.clear();
+
+const starContainer = document.querySelector('[data-js="star-container"]');
+
+//1. render a single star (create img and append)
+//2. use a for loop to do the same 5 times
+
+const singleStar
+
+function renderStars() {
+  // Reset the star container before re-rendering stars
+  starContainer.innerHTML = "";
+
+  // --v-- write or modify code below this line --v--
+
+  // --^-- write or modify code above this line --^--
+}
+
+renderStars();
