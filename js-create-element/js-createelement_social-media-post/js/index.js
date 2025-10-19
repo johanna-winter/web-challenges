0c27b8e0edf69702b71718cdede910fb7ae3cc96
Johanna Winter <johanna.winter@gmx.de>
2025-10-19 15:36:11 +0200

    add second social media post

--- js-create-element/js-createelement_social-media-post/js/index.js
+++ js-create-element/js-createelement_social-media-post/js/index.js
@@ -8,5 +8,31 @@ function handleLikeButtonClick(event) {
 const likeButton = document.querySelector('[data-js="like-button"]');
 likeButton.addEventListener("click", handleLikeButtonClick);
 
+const secondPost = document.createElement("article");
+secondPost.classList.add("post");
+document.body.append(secondPost);
+
+const secondPostContent = document.createElement("p");
+secondPostContent.classList.add("post__content");
+secondPostContent.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+secondPost.append(secondPostContent);
+
+const secondPostFooter = document.createElement("footer");
+secondPostFooter.classList.add("post__footer");
+secondPost.append(secondPostFooter);
+
+const secondPostUsername = document.createElement("span");
+secondPostUsername.classList.add("post__username");
+secondPostUsername.textContent = "@username";
+secondPostFooter.append(secondPostUsername);
+
+const secondPostButton = document.createElement("button");
+secondPostButton.classList.add("post__button");
+secondPostButton.textContent = "♥ Like";
+secondPostFooter.append(secondPostButton);
+
+secondPostButton.addEventListener("click", handleLikeButtonClick);
+
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
