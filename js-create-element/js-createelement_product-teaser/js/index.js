30710f0f773322973e6f14cc2a3b252f98e8ea9c
Johanna Winter <johanna.winter@gmx.de>
2025-10-19 16:24:17 +0200

    add solution

--- js-create-element/js-createelement_product-teaser/js/index.js
+++ js-create-element/js-createelement_product-teaser/js/index.js
@@ -9,3 +9,57 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+const article = document.createElement("article");
+const productBody = document.createElement("section");
+const textContainer = document.createElement("div");
+const productName = document.createElement("h2");
+const productCategories = document.createElement("ul");
+const productCategory1 = document.createElement("li");
+const productCategory2 = document.createElement("li");
+const productCategory3 = document.createElement("li");
+const productDescription = document.createElement("p");
+const imageContainer = document.createElement("div");
+const productImage = document.createElement("img");
+const imageSource = document.createElement("src");
+const footer = document.createElement("footer");
+const priceDisplay = document.createElement("span");
+const buyButton = document.createElement("button");
+
+article.classList.add("product");
+productBody.classList.add("product__body");
+textContainer.classList.add("product__text-container");
+productName.classList.add("product__name");
+productCategories.classList.add("product__categories");
+productCategory1.classList.add("product__category");
+productCategory2.classList.add("product__category");
+productCategory3.classList.add("product__category");
+productDescription.classList.add("product__description");
+imageContainer.classList.add("product__image-container");
+footer.classList.add("product__footer");
+priceDisplay.classList.add("product__price");
+buyButton.classList.add("product__buy-button");
+
+document.body.append(article);
+article.append(productBody, footer);
+productBody.append(textContainer, imageContainer);
+textContainer.append(productName, productCategories, productDescription);
+productCategories.append(productCategory1, productCategory2, productCategory3);
+imageContainer.append(productImage);
+productImage.append(imageSource);
+footer.append(priceDisplay, buyButton);
+
+productDescription.innerHTML = description;
+productName.innerHTML = name;
+productCategory1.innerHTML = category1;
+productCategory2.innerHTML = category2;
+productCategory3.innerHTML = category3;
+imageContainer.innerHTML = `<img class="product__image" src="${imageSrc}">`;
+priceDisplay.innerHTML = price;
+buyButton.innerHTML = "Buy";
+
+const button = article.querySelector("button");
+button.addEventListener("click", () => {
+  console.log("Name: " + name);
+  console.log("Price: " + price);
+});
