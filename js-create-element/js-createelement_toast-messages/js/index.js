40f792afd38015d0b9ea2d31f93269240eb6a185
Johanna Winter <johanna.winter@gmx.de>
2025-10-19 15:13:56 +0200

    add button functionalities

--- js-create-element/js-createelement_toast-messages/js/index.js
+++ js-create-element/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,15 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.classList.add("toast-container__message");
+  li.textContent = "I'm a toast message.";
+  toastContainer.append(li);
   // Exercise: Append a new entry to the toast messages container
 });
 
 clearButton.addEventListener("click", () => {
+  const removeMessage = document.querySelector('[data-js="toast-container"]');
+  removeMessage.innerHTML = "";
   // Exercise: Clear the stack of toast messages
 });
