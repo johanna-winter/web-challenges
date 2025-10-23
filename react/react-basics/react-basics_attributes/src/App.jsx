20bdcaedd09120e3519055100db7d608140147eb
Johanna Winter <johanna.winter@gmx.de>
2025-10-23 16:58:28 +0200

    add article component

--- react/react-basics/react-basics_attributes/src/App.jsx
+++ react/react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,23 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">Super creative headline</h2>
+      <label htmlFor="input-search">
+        Search:&nbsp;
+        <input id="input-search" type="text"></input>&nbsp;
+      </label>
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/Golden_Retriever"
+      >
+        Learn more
+      </a>
+    </article>
+  );
 }
