c3ffce6683a5a201eced1187813a7e41834e331f
Johanna Winter <johanna.winter@gmx.de>
2025-10-24 12:19:38 +0200

    add solution

--- react/react-props/react-props_buttons/src/App.jsx
+++ react/react-props/react-props_buttons/src/App.jsx
@@ -1,3 +1,34 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("Hello world!");
+    alert("You clicked me!");
+  }
+  return (
+    <Button
+      color="#3f0425"
+      text="Click me"
+      disabled="false"
+      onClick={handleClick}
+    />
+  );
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <>
+      <button
+        style={{
+          height: "50px",
+          width: "100px",
+          borderRadius: "8px",
+          backgroundColor: color,
+          color: "#fff",
+          disabled: { disabled },
+        }}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    </>
+  );
 }
