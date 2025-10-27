e68f7d64ab596783af27469b5da793184c385482
Johanna Winter <johanna.winter@gmx.de>
2025-10-27 11:48:29 +0100

    refactoring code

--- react/react-state1/react-state-1_box/src/App.jsx
+++ react/react-state1/react-state-1_box/src/App.jsx
@@ -1,18 +1,27 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  let isActive = false;
+  // let isActive = false;
+  const [isActive, setIsActive] = useState(false);
 
   function handleClick() {
-    isActive = !isActive;
+    setIsActive(!isActive);
     // Check that the value changes correctly.
     console.log(isActive);
   }
 
+  // function handleText() {
+  //   setText(!text);
+  //   console.log(text);
+  // }
+
   return (
     <main>
       <div className={`box ${isActive ? "box--active" : ""}`} />
-      <button onClick={handleClick}>Activate</button>
+      <button onClick={handleClick}>
+        {isActive ? "Deactivate" : "Activate"}
+      </button>
     </main>
   );
 }
