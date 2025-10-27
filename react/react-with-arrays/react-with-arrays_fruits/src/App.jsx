14205c7156adc3b8745bf853967d092680c68bab
Johanna Winter <johanna.winter@gmx.de>
2025-10-27 15:47:10 +0100

    use map method

--- react/react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react/react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,23 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    { id: 1337, name: "🍌 banana", color: "yellow" },
+    { id: 293, name: "🍊 orange", color: "orange" },
+    { id: 1586, name: "🍓 strawberry", color: "red" },
+    { id: 789, name: "🥝 kiwi", color: "green" },
+    { id: 99, name: "🍎 apple", color: "red" },
+  ];
 
-  return (
-    <div className="app">
-      <Card name="🍌 banana" />
-    </div>
-  );
+  const fruitCards = fruits.map((fruit) => {
+    return (
+      <Card
+        key={fruit.id}
+        id={fruit.id}
+        name={fruit.name}
+        color={fruit.color}
+      />
+    );
+  });
+  return <div className="app">{fruitCards}</div>;
 }
