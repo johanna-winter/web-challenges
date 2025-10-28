f8fdf97b425ef01ff290fe2ccff7520e56f03db3
Johanna Winter <johanna.winter@gmx.de>
2025-10-28 15:42:49 +0100

    add and delete functions

--- react/react-state3/react-state-3_tags/src/App.jsx
+++ react/react-state3/react-state-3_tags/src/App.jsx
@@ -6,10 +6,19 @@ import List from "./components/List";
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, newTag]);
+  }
+
+  function handleDeleteTag(tagToDelete) {
+    const updatedTags = tags.filter((tag) => tag !== tagToDelete);
+    setTags(updatedTags);
+  }
+
   return (
     <main className="app">
-      <Form />
-      <List tags={tags} />
+      <Form onAddTag={handleAddTag} />
+      <List tags={tags} onDeleteTag={handleDeleteTag} />
     </main>
   );
 }
