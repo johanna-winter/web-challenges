1ace87a739e2c602f0539474462146a2b3db70c7
Johanna Winter <johanna.winter@gmx.de>
2025-10-28 13:16:05 +0100

    add solution

--- react/react-state2/react-state-2_personal-details/src/components/Form.jsx
+++ react/react-state2/react-state-2_personal-details/src/components/Form.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
-
-export default function Form() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-
+export default function Form({ onSubmit }) {
   function handleSubmit(event) {
     event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log("Name: ", data.name);
+    console.log("Email: ", data.email);
+
+    // setName(data.name);
+    // setEmail(data.email);
+
+    onSubmit(data.name, data.email);
+
+    event.target.reset();
+    event.target.elements.name.focus();
   }
 
   return (
@@ -15,9 +23,9 @@ export default function Form() {
       onSubmit={handleSubmit}
     >
       <h2 id="user-details">Please enter your details here!</h2>
-      <label htmlFor="name">Name: </label>
+      <label htmlFor="name">Name:</label>
       <input id="name" name="name" type="text" placeholder="John Doe" />
-      <label htmlFor="email">Email: </label>
+      <label htmlFor="email">Email:</label>
       <input id="email" name="email" type="email" placeholder="john@doe.com" />
       <button className="form__submit-button" type="submit">
         Submit
