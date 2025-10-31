d521e046baf6ea1630bbe13c9aa036f020595c1f
Johanna Winter <johanna.winter@gmx.de>
2025-10-31 16:24:42 +0100

    add solution

--- react/react-custom-hooks/react-custom-hooks-usemouse/src/hooks/useMouse.js
+++ react/react-custom-hooks/react-custom-hooks-usemouse/src/hooks/useMouse.js
@@ -1,1 +1,15 @@
-export default function useMouse() {}
+import { useEffect, useState } from "react";
+
+export default function useMouse() {
+  const [cursorPosition, setCursorPosition] = useState([0, 0]);
+  useEffect(() => {
+    function handleMouseMove(event) {
+      setCursorPosition([event.clientX, event.clientY]);
+    }
+
+    document.body.addEventListener("mousemove", handleMouseMove);
+    console.log("Mouse position: ", cursorPosition);
+  }, [cursorPosition]);
+
+  return cursorPosition;
+}
