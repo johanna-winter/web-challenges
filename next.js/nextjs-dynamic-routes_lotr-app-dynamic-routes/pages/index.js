46cf8f12899177eb86a038d5dccf05c3da2eb678
Johanna Winter <johanna.winter@gmx.de>
2025-11-07 16:54:23 +0100

    add solution

--- next.js/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/index.js
+++ next.js/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/index.js
@@ -1,9 +1,29 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { volumes } from "@/lib/data";
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+const randomElement = getRandomElement(volumes);
+console.log("Random volume: ", randomElement);
 
 export default function HomePage() {
+  const router = useRouter();
   return (
     <div>
       <Link href="/volumes">Go to all volumes</Link>
+      <br />
+      <br />
+      <button
+        type="button"
+        onClick={() => {
+          router.push(`/volumes/${randomElement.slug}`);
+        }}
+      >
+        Get random volume
+      </button>
     </div>
   );
 }
