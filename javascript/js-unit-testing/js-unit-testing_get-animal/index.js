cf22cfbf9a53bfd0838d9eceaad8c85aa56a607f
Johanna Winter <johanna.winter@gmx.de>
2025-11-10 15:43:48 +0100

    add getAnimal function

--- javascript/js-unit-testing/js-unit-testing_get-animal/index.js
+++ javascript/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,13 @@
-export function getAnimal() {}
+export function getAnimal(animal = "") {
+  if (!animal) {
+    return `I do not like animals at all!`;
+  }
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  }
+  if (animal === "dogs") {
+    return `I like ${animal}!`;
+  }
+}
+
+console.log(getAnimal("dogs"));
