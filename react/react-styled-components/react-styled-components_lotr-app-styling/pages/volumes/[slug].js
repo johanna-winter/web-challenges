2ee5a2ed7ad816728d862381ab16ac3e194e4e53
Johanna Winter <johanna.winter@gmx.de>
2025-11-16 23:08:21 +0100

    add styling

--- react/react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
+++ react/react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
@@ -2,6 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { volumes } from "../../lib/data";
+import styled, { css } from "styled-components";
+import ArrowLeft from "../../public/images/arrow-left.svg";
+import ArrowRight from "../../public/images/arrow-right.svg";
+import ChevronLeft from "../../public/images/chevron-left.svg";
 
 export default function VolumeDetail() {
   const router = useRouter();
@@ -17,40 +21,163 @@ export default function VolumeDetail() {
     return null;
   }
 
-  const { title, description, cover, books } = volume;
+  const { title, description, cover, books, color } = volume;
 
   return (
-    <>
-      <Link href="/volumes">← All Volumes</Link>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <ul>
-        {books.map(({ ordinal, title }) => (
-          <li key={title}>
-            {ordinal}: <strong>{title}</strong>
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={cover}
-        alt={`Cover image of ${title}`}
-        width={140}
-        height={230}
-      />
-      {previousVolume ? (
-        <div>
-          <Link href={`/volumes/${previousVolume.slug}`}>
-            ← Previous Volume: {previousVolume.title}
-          </Link>
-        </div>
-      ) : null}
-      {nextVolume ? (
-        <div>
-          <Link href={`/volumes/${nextVolume.slug}`}>
-            Next Volume: {nextVolume.title} →
-          </Link>
-        </div>
-      ) : null}
-    </>
+    <Container>
+      <AllVolumesLink href="/volumes">
+        {" "}
+        <ChevronLeft />
+        All Volumes
+      </AllVolumesLink>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+      <BookVolume $color={color}>
+        <BookList>
+          {books.map(({ ordinal, title }) => (
+            <li key={title}>
+              <BookOrdinal>{ordinal}:</BookOrdinal>
+              <BookTitle>{title}</BookTitle>
+            </li>
+          ))}
+        </BookList>
+        <Book
+          src={cover}
+          alt={`Cover image of ${title}`}
+          width={140}
+          height={230}
+        />
+      </BookVolume>
+      <Navigation>
+        {previousVolume ? (
+          <NavLink href={`/volumes/${previousVolume.slug}`} $position="left">
+            <ArrowLeft />
+            <div>
+              <VolumeHeading>Previous Volume</VolumeHeading>{" "}
+              <TitleLink>{previousVolume.title}</TitleLink>
+            </div>
+          </NavLink>
+        ) : null}
+        {nextVolume ? (
+          <NavLink href={`/volumes/${nextVolume.slug}`} $position="right">
+            <div>
+              <VolumeHeading>Next Volume</VolumeHeading>{" "}
+              <TitleLink>{nextVolume.title}</TitleLink>
+            </div>
+            <ArrowRight />
+          </NavLink>
+        ) : null}
+      </Navigation>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  padding: 2rem;
+`;
+
+const AllVolumesLink = styled(Link)`
+  text-decoration: none;
+  color: unset;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const Title = styled.h1`
+  font: var(--font-headline-1);
+`;
+
+const Description = styled.p`
+  font: var(--font-body);
+  font-weight: normal;
+  line-height: 1.5rem;
+  padding: 0.75 0;
+`;
+
+const BookList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2rem;
+`;
+
+const BookOrdinal = styled.p`
+  font: var(--font-caption-italic);
+  margin: 0;
+  margin-bottom: 8px;
+`;
+
+const BookTitle = styled.strong`
+  font: var(--font-title);
+`;
+
+const Book = styled(Image)`
+  box-shadow: var(--box-shadow-book);
+  &:hover {
+    box-shadow: var(--box-shadow-book--hover);
+  }
+`;
+
+const BookVolume = styled.div`
+  color: var(--color-smoke);
+  margin-inline: -2rem;
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+  padding: 1.5rem;
+  background: linear-gradient(
+    100deg,
+    ${({ $color }) =>
+      `${$color} 40%, color-mix(in lch, ${$color}, var(--color-smoke)) 140%`}
+  );
+  margin-bottom: 1.5rem;
+`;
+
+const Navigation = styled.nav`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-areas: "prev" "next";
+  margin-top: 2rem;
+  gap: 8px;
+  align-items: center;
+  font-weight: normal;
+`;
+const NavLink = styled(Link)`
+  text-decoration: none;
+  color: inherit;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  ${({ $position }) =>
+    $position === "left" &&
+    css`
+      grid-area: prev;
+      justify-self: start;
+      text-align: left;
+    `}
+  ${({ $position }) =>
+    $position === "right" &&
+    css`
+      grid-area: next;
+      justify-self: end;
+      text-align: right;
+    `};
+`;
+
+const VolumeHeading = styled.p`
+  font: var(--font-caption--italic);
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 1.2rem;
+  margin: 0;
+`;
+
+const TitleLink = styled.p`
+  font: var(--font-caption);
+  font-weight: bold;
+  margin: 0;
+`;
--- react/react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/index.js
+++ react/react-styled-components/react-styled-components_lotr-app-styling/pages/volumes/index.js
@@ -1,18 +1,68 @@
 import Link from "next/link";
 import { introduction, volumes } from "../../lib/data";
+import styled from "styled-components";
+import Image from "next/image";
 
 export default function Volumes() {
   return (
-    <>
-      <h1>The Lord of the Rings</h1>
-      <p>{introduction}</p>
-      <ul>
+    <Container>
+      <Title>The Lord of the Rings</Title>
+      <Description>{introduction}</Description>
+      <VolumeTitle>All Volumes</VolumeTitle>
+      <VolumeList>
         {volumes.map((volume) => (
           <li key={volume.id}>
-            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+            <Image
+              src={volume.cover}
+              alt={`Cover image of ${volume.title}`}
+              width={80}
+              height={130}
+            />
+            <StyledLink href={`/volumes/${volume.slug}`}>
+              <Volume>{volume.title}</Volume>
+            </StyledLink>
           </li>
         ))}
-      </ul>
-    </>
+      </VolumeList>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  padding: 2rem;
+`;
+
+const Title = styled.h1`
+  font: var(--font-headline-1);
+`;
+
+const Description = styled.p`
+  font: var(--font-body);
+  padding-block: 1rem;
+`;
+
+const VolumeTitle = styled.h2`
+  font: var(--font-headline-2);
+`;
+
+const VolumeList = styled.ul`
+  list-style: none;
+  display: grid;
+  padding: 0;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+  justify-items: center;
+`;
+
+const Volume = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  width: 80px;
+`;
+
+const StyledLink = styled(Link)`
+  color: inherit;
+  text-decoration: none;
+  font: var(--font-caption);
+`;
