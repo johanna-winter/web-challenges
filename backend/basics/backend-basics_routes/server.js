3c7f6e3f56298ae922c7943202abbe0cc5c24414
Johanna Winter <johanna.winter@gmx.de>
2025-11-20 16:10:13 +0100

    add solution

--- backend/basics/backend-basics_routes/server.js
+++ backend/basics/backend-basics_routes/server.js
@@ -1,3 +1,15 @@
 import { createServer } from "node:http";
 
-export const server = createServer();
+export const server = createServer((request, response) => {
+  console.log(request.url);
+  if (request.url === "/api/fish/1") {
+    response.statusCode = 200;
+    response.end("Shrimp");
+  } else if (request.url === "/api/fish/2") {
+    response.statusCode = 200;
+    response.end("Anemonefish");
+  } else {
+    response.statusCode = 404;
+    response.end("Not found");
+  }
+});
